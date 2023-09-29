736563ff357c6cd3769610c8728380280041b498
Konrad Iwan <kon.iwan17@gmail.com>
2023-09-29 21:50:12 +0200

    creating simple footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { IconBrandGithubFilled } from "@tabler/icons-react";
+
+const Footer = () => {
+  return (
+    <footer className="mt-auto w-full p-2 dark:bg-primary-950 dark:text-primary xl:rounded-md">
+      <div className="flex w-full max-w-screen-xl items-center">
+        <span className="flex items-center">
+          aplikacja stworzona przez
+          <a
+            className="ml-2 flex underline"
+            href="https://github.com/sheep2moon"
+            target="_blank"
+          >
+            <IconBrandGithubFilled className="text-primary-600 dark:text-primary" />
+            sheep2moon
+          </a>
+        </span>
+        <Button variant="outline" className="ml-auto">
+          Zgłoś błąd/sugestie
+        </Button>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroBanner from "../components/homepage/HeroBanner";
 import RecentFisheries from "../components/homepage/RecentFisheries";
+import Footer from "../components/footer/footer";
 
 const Home = () => {
   return (
-    <div className="mt-16 flex w-full flex-col items-center justify-center text-black">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center pt-16 text-black">
       <HeroBanner />
       <RecentFisheries />
+      <Footer />
     </div>
   );
 };
